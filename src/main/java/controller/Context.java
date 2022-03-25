72559c4e7454c2b9022b8496639f980709293c60
PC53 <s2104454@ed.ac.uk>
2022-03-25 13:52:30 +0000

    constructor for context.java and Controller

--- src/main/java/controller/Context.java
+++ src/main/java/controller/Context.java
@@ -1,4 +1,50 @@
 package controller;
 
-public class Context {
+import external.MockPaymentSystem;
+import external.PaymentSystem;
+import state.*;
+
+public class Context extends Object{
+    private PaymentSystem mockPaymentSystem;
+    private IUserState userState;
+    private IEventState eventState;
+    private IBookingState bookingState;
+    private ISponsorshipState sponsorshipState;
+
+    private ISponsorshipState attribute;
+
+    public Context(){
+        this.mockPaymentSystem = new MockPaymentSystem();
+        this.userState = new UserState();
+        this.eventState = new EventState();
+        this.bookingState = new BookingState();
+        this.sponsorshipState = new SponsorshipState();
+
+    }
+
+    public Context(Context other){
+        this.mockPaymentSystem = other.getPaymentSystem();
+
+    }
+
+    public PaymentSystem getPaymentSystem(){
+        return mockPaymentSystem;
+    }
+
+    public IUserState getUserState(){
+        return userState;
+    }
+
+    public IBookingState getBookingState(){
+        return bookingState;
+    }
+
+    public IEventState getEventState(){
+        return eventState;
+    }
+
+    public ISponsorshipState getSponsorshipState(){
+        return sponsorshipState;
+    }
+
 }
