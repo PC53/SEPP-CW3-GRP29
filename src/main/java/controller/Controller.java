5b39626855f156f7ab8d24ef62dcb1c3ff6a75b4
PC53 <s2104454@ed.ac.uk>
2022-03-25 14:31:19 +0000

    Changes by andre

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,21 @@
+package controller;
+
+import state.IEventState;
+import state.ISponsorshipState;
+import state.IUserState;
+import state.IBookingState;
+
+public class Controller extends Object {
+
+    /* private PaymentSystem paymentSystem;
+    private IUserState userState;
+    private IEventState eventState;
+    private IBookingState bookingState;
+
+    private ISponsorshipState sponsorshipState;
+
+    private ISponsorshipState attribute; */
+
+
+
+}
