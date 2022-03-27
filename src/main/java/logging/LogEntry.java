120bbdff5906d6e4c94993fa3e63c829f58e7f82
Q1ngWen <s2095157@ed.ac.uk>
2022-03-27 20:51:06 +0100

    recommit command bottom part and logging completed

--- src/main/java/logging/LogEntry.java
+++ src/main/java/logging/LogEntry.java
@@ -1,4 +1,26 @@
 package logging;
 
-public class LogEntry {
+import java.util.Map;
+
+public class LogEntry extends Object{
+
+    private String callerName;
+    private Object result;
+    private Map<String, Object> additionalInfo;
+
+    public LogEntry(String callerName,
+                    Object result,
+                    Map<String, Object> additionalInfo){
+        this.callerName = callerName;
+        this.result = result;
+        this.additionalInfo = additionalInfo;
+    }
+
+    public String getResult() {
+        return null;
+    }
+
+    public String toString(){
+        return null;
+    }
 }
