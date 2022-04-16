d6dfdf3036ce2a890ffc4ee3a36c9f23539587e9
Stephen Shen <s2023501@ed.ac.uk>
2022-04-16 15:45:41 +0100

    Fixing GovernmentReport1 command

--- src/main/java/command/GovernmentReport1Command.java
+++ src/main/java/command/GovernmentReport1Command.java
@@ -23,26 +23,41 @@ public class GovernmentReport1Command extends Object implements ICommand{
 
     @Override
     public void execute(Context context) {
+        User user = context.getUserState().getCurrentUser();
+
         if(intervalEndInclusive.isBefore(intervalStartInclusive) || intervalEndInclusive.isEqual(intervalStartInclusive)){
             return;
         }
-        // looping through all the events adding bookings in the interval
-        for (Event event : context.getEventState().getAllEvents()){
-            if(event instanceof TicketedEvent) {
-                long eventNumber = event.getEventNumber();
 
-                List<Booking> bookings = context.getBookingState().findBookingsByEventNumber(eventNumber);
-                for (Booking booking : bookings) {
-                    EventPerformance ep = booking.getEventPerformance();
-                    if(!ep.getStartDateTime().isBefore(intervalStartInclusive)
-                            && !ep.getEndDateTime().isAfter(intervalEndInclusive)
-                            && booking.getStatus() == BookingStatus.Active){
-                        output.add(booking);
+        if (user instanceof GovernmentRepresentative) {
+            // looping through all the events adding bookings in the interval
+            for (Event event : context.getEventState().getAllEvents()) {
+                long eventNumber = event.getEventNumber();
+                boolean status = false;
+                List<SponsorshipRequest> requests = context.getSponsorshipState().getAllSponsorshipRequests();
+                for (SponsorshipRequest request : requests) {
+                    if (request.getEvent().getEventNumber() == eventNumber) {
+                        if (request.getStatus() == SponsorshipStatus.ACCEPTED) {
+                            status = true;
+                            break;
+                        }
+                    }
+                }
+                if (event instanceof TicketedEvent && status) {
+                    List<Booking> bookings = context.getBookingState().findBookingsByEventNumber(eventNumber);
+                    for (Booking booking : bookings) {
+                        EventPerformance ep = booking.getEventPerformance();
+                        if (!ep.getStartDateTime().isBefore(intervalStartInclusive)
+                                && !ep.getEndDateTime().isAfter(intervalEndInclusive)
+                                && booking.getStatus() == BookingStatus.Active) {
+                            output.add(booking);
+                        }
                     }
                 }
             }
+        } else {
+            return;
         }
-
     }
 
     @Override
