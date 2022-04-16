09ee445f1887fb5a03d4590ac6ba4f9d403305cc
Stephen Shen <s2023501@ed.ac.uk>
2022-04-16 15:49:58 +0100

    Add test file for BookingState unit tests

---
+++ src/test/java/TestBookingState.java
@@ -0,0 +1,17 @@
+import logging.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+
+public class TestBookingState {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---");
+    }
+}
