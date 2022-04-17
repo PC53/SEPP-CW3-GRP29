5c573ea25e6ac194594e629b4ecea38eeccde309
Stephen Shen <s2023501@ed.ac.uk>
2022-04-17 15:24:18 +0100

    Edited system tests of ViewEvent command.

--- src/test/java/ViewEventSystemTests.java
+++ src/test/java/ViewEventSystemTests.java
@@ -14,7 +14,6 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ViewEventSystemTests {
     @BeforeEach
