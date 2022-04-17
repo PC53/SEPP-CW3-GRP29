2dbfff55a008a7c2f229c40f07486a584df05bdc
Stephen Shen <s2023501@ed.ac.uk>
2022-04-17 16:52:37 +0100

    Update system tests for AcceptOrRejectSponsorship command.

--- src/test/java/AcceptOrRejectSponsorshipSystemTests.java
+++ src/test/java/AcceptOrRejectSponsorshipSystemTests.java
@@ -1,19 +1,14 @@
-import command.CancelEventCommand;
-import command.CreateTicketedEventCommand;
-import command.RegisterEntertainmentProviderCommand;
-import command.RespondSponsorshipCommand;
+import command.*;
 import controller.Controller;
 import logging.Logger;
-import model.EntertainmentProvider;
-import model.EventType;
-import model.SponsorshipRequest;
-import model.TicketedEvent;
+import model.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -50,6 +45,18 @@ public class AcceptOrRejectSponsorshipSystemTests {
         ));
     }
 
+    private static void govRepLogIn(Controller controller) {
+        controller.runCommand(new LoginCommand("margaret.thatcher@gov.uk", "The Good times  "));
+    }
+
+    private static void EntProviderLogIn(Controller controller) {
+        controller.runCommand(new LoginCommand("joeorg@gmail.com", "iloveorgs"));
+    }
+
+    private static void logOutCommand(Controller controller) {
+        controller.runCommand(new LogoutCommand());
+    }
+
     @Test
     void acceptSponsorshipRequest() {
         Controller controller = new Controller();
@@ -57,26 +64,7 @@ public class AcceptOrRejectSponsorshipSystemTests {
         // create event, set sponsorship request
         // respond to request
 
-        ArrayList<String> orgReps = new ArrayList<String>();
-        orgReps.add("Jane Org");
-        orgReps.add("Josh Org");
-        ArrayList<String> orgRepsEmails = new ArrayList<String>();
-        orgRepsEmails.add("janeorg@gmail.com");
-        orgRepsEmails.add("joshorg@gmail.com");
-
-        RegisterEntertainmentProviderCommand cmd0 = new RegisterEntertainmentProviderCommand(
-                "entertainmentOrgOne",
-                "22 Org Street, OrgCity, Scotland",
-                "orgone@gmail.com",
-                "Joe Org",
-                "joeorg@gmail.com",
-                "iloveorgs",
-                orgReps,
-                orgRepsEmails
-        );
-
-        controller.runCommand(cmd0);
-        EntertainmentProvider ep = cmd0.getResult();
+        registerEntProvider1(controller);
 
         CreateTicketedEventCommand cmd1 = new CreateTicketedEventCommand(
                 "ticketed event one",
@@ -88,29 +76,40 @@ public class AcceptOrRejectSponsorshipSystemTests {
         controller.runCommand(cmd1);
         long eventNum = cmd1.getResult();
 
-        TicketedEvent testEvent = new TicketedEvent(
-                eventNum,
-                ep,
-                "test ticketed event 1",
-                EventType.Movie,
-                10,
-                9999
-        );
+        logOutCommand(controller);
 
-        SponsorshipRequest testSponsorshipRequest = new SponsorshipRequest(1, testEvent);
+        govRepLogIn(controller);
 
-        RespondSponsorshipCommand cmd3 = new RespondSponsorshipCommand(
-                testSponsorshipRequest.getRequestNumber(),
+        RespondSponsorshipCommand cmd2 = new RespondSponsorshipCommand(
+                1,
                 10
         );
 
-        boolean successfulResponse = cmd3.getResult();
+        controller.runCommand(cmd2);
+        boolean successfulResponse = cmd2.getResult();
+
+        logOutCommand(controller);
+
+        EntProviderLogIn(controller);
+
+        ListEventsCommand cmd3 = new ListEventsCommand(false, false);
+        controller.runCommand(cmd3);
+        List<Event> listofevents = cmd3.getResult();
+
+        logOutCommand(controller);
+
+        Event testEvent = listofevents.get(0);
+
         assertEquals(true, successfulResponse);
 
-        assertEquals(true, testEvent.isSponsored());
-        assertNotEquals(testEvent.getOriginalTicketPrice(), testEvent.getDiscountedTicketPrice());
+        assertEquals(true, ((TicketedEvent)testEvent).isSponsored());
+        assertNotEquals(((TicketedEvent)testEvent).getOriginalTicketPrice(),
+                        ((TicketedEvent)testEvent).getDiscountedTicketPrice()
+        );
+        assertEquals(0.9,
+                     (((TicketedEvent)testEvent).getDiscountedTicketPrice() / ((TicketedEvent)testEvent).getOriginalTicketPrice())
+        );
 
-        // could also find a way to check ticket discount is exactly 10%
     }
 
     @Test
@@ -120,26 +119,7 @@ public class AcceptOrRejectSponsorshipSystemTests {
         // create event, set sponsorship request
         // respond to request (deny)
 
-        ArrayList<String> orgReps = new ArrayList<String>();
-        orgReps.add("Jane Org");
-        orgReps.add("Josh Org");
-        ArrayList<String> orgRepsEmails = new ArrayList<String>();
-        orgRepsEmails.add("janeorg@gmail.com");
-        orgRepsEmails.add("joshorg@gmail.com");
-
-        RegisterEntertainmentProviderCommand cmd0 = new RegisterEntertainmentProviderCommand(
-                "entertainmentOrgOne",
-                "22 Org Street, OrgCity, Scotland",
-                "orgone@gmail.com",
-                "Joe Org",
-                "joeorg@gmail.com",
-                "iloveorgs",
-                orgReps,
-                orgRepsEmails
-        );
-
-        controller.runCommand(cmd0);
-        EntertainmentProvider ep = cmd0.getResult();
+        registerEntProvider1(controller);
 
         CreateTicketedEventCommand cmd1 = new CreateTicketedEventCommand(
                 "ticketed event one",
@@ -151,27 +131,36 @@ public class AcceptOrRejectSponsorshipSystemTests {
         controller.runCommand(cmd1);
         long eventNum = cmd1.getResult();
 
-        TicketedEvent testEvent = new TicketedEvent(
-                eventNum,
-                ep,
-                "test ticketed event 1",
-                EventType.Movie,
-                10,
-                9999
-        );
+        logOutCommand(controller);
 
-        SponsorshipRequest testSponsorshipRequest = new SponsorshipRequest(1, testEvent);
+        govRepLogIn(controller);
 
-        RespondSponsorshipCommand cmd3 = new RespondSponsorshipCommand(
-                testSponsorshipRequest.getRequestNumber(),
+        RespondSponsorshipCommand cmd2 = new RespondSponsorshipCommand(
+                1,
                 0
         );
 
-        boolean successfulResponse = cmd3.getResult();
+        controller.runCommand(cmd2);
+        boolean successfulResponse = cmd2.getResult();
+
+        logOutCommand(controller);
+
+        EntProviderLogIn(controller);
+
+        ListEventsCommand cmd3 = new ListEventsCommand(false, false);
+        controller.runCommand(cmd3);
+        List<Event> listofevents = cmd3.getResult();
+
+        logOutCommand(controller);
+
+        Event testEvent = listofevents.get(0);
+
         assertEquals(true, successfulResponse);
 
-        assertEquals(false, testEvent.isSponsored());
-        assertEquals(testEvent.getOriginalTicketPrice(), testEvent.getDiscountedTicketPrice());
+        assertEquals(false, ((TicketedEvent)testEvent).isSponsored());
+        assertEquals(((TicketedEvent)testEvent).getOriginalTicketPrice(),
+                     ((TicketedEvent)testEvent).getDiscountedTicketPrice()
+        );
 
     }
 
